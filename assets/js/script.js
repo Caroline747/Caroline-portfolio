5e3d6344eab3ba3b7092d30e8f353381802c75b4
Caroline <karolsamara97@gmail.com>
2025-08-30 13:30:22 -0300

    up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
 // Inicialize com seu User ID (do painel da EmailJS)
-    emailjs.init("pjGsV2By3WOJKtM28");
+emailjs.init("pjGsV2By3WOJKtM28");
 
-    const form = document.getElementById("contactForm");
-    const msgBox = document.getElementById("formMsg");
-    const btn = form.querySelector("button");
+const form = document.getElementById("contactForm");
+const msgBox = document.getElementById("formMsg");
+const btn = form.querySelector("button");
 
-    form.addEventListener("submit", function(e) {
-      e.preventDefault();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-      msgBox.textContent = "Enviando...";
-      msgBox.className = "msg";
-      btn.disabled = true;
+  msgBox.textContent = "Enviando...";
+  msgBox.className = "msg";
+  btn.disabled = true;
 
-      emailjs.sendForm("service_lex4xjn", "template_b4fovbc", this)
-        .then(() => {
-          msgBox.textContent = "Mensagem enviada com sucesso!";
-          msgBox.className = "msg success";
-          form.reset();
-        })
-        .catch(err => {
-          console.error("Erro:", err);
-          msgBox.textContent = "Erro ao enviar. Tente novamente.";
-          msgBox.className = "msg error";
-        })
-        .finally(() => {
-          btn.disabled = false;
-        });
+  emailjs.sendForm("service_lex4xjn", "template_b4fovbc", this)
+    .then(() => {
+      msgBox.textContent = "Mensagem enviada com sucesso!";
+      msgBox.className = "msg success";
+      form.reset();
+    })
+    .catch(err => {
+      console.error("Erro:", err);
+      msgBox.textContent = "Erro ao enviar. Tente novamente.";
+      msgBox.className = "msg error";
+    })
+    .finally(() => {
+      btn.disabled = false;
     });
+});
